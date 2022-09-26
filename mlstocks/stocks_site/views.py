082dcc719dca825065fb70ef3e1ffce62fb94819
njwicker <njwicker@uwm.edu>
2022-09-26 11:24:51 -0500

    Added forgetPass.html anf createAccount.html

--- mlstocks/stocks_site/views.py
+++ mlstocks/stocks_site/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login_view(request):
     return render(request,'stocks_site/login.html')
+
+def forget_pass(request):
+    return render(request,'stocks_site/forgetPass.html')
